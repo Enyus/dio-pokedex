6a9fb784aebe95cda4bbaafbbe9bca3d7314916b
Cassio Sandrigo Santini <cassio.santini@educacao.sp.gov.br>
2023-08-31 13:44:15 -0300

    começando a povoar a modal, falta evolução

--- assets/js/modalPokemon.js
+++ assets/js/modalPokemon.js
@@ -1,12 +1,85 @@
 const modal = document.getElementById("modalPokemon");
 const botaoFecharModal = document.getElementById("botaoFecharModal");
-// modal.close()
+const arrayStatsTitulo = Array.from(document.getElementsByClassName("stats__item--titulo"));
+const arrayAbasDesc = Array.from(document.getElementsByClassName("description__aba"));
 
-function abrirModalDoPokemon(numeroDoPokemon) {
+const nomeDoPokemon = document.getElementById("nomeDoPokemon");
+const numeroDoPokemon = document.getElementById("numeroDoPokemon")
+const tiposDoPokemon = document.getElementById("tiposDoPokemon")
+const imagemDoPokemon = document.getElementById("imagemDoPokemon")
+const especieDoPokemon = document.getElementById("especieDoPokemon")
+const alturaDoPokemon = document.getElementById("alturaDoPokemon")
+const pesoDoPokemon = document.getElementById("pesoDoPokemon")
+const habilidadesDoPokemon = document.getElementById("habilidadesDoPokemon")
+const vidaDoPokemon = document.getElementById("vidaDoPokemon")
+const ataqueDoPokemon = document.getElementById("ataqueDoPokemon")
+const defesaDoPokemon = document.getElementById("defesaDoPokemon")
+const spatkDoPokemon = document.getElementById("spatkDoPokemon")
+const spdefDoPokemon = document.getElementById("spdefDoPokemon")
+const velDoPokemon = document.getElementById("velDoPokemon")
+const imagemPokemonAntesEvo = document.getElementById("imagemPokemonAntesEvo")
+const imagemPokemonDepoisEvo = document.getElementById("imagemPokemonDepoisEvo")
+const nomeDaEvoDoPokemon = document.getElementById("nomeDaEvoDoPokemon");
+
+function abrirModalDoPokemon(idDoPokemon) {
     modal.showModal();
-    console.log(`abriu a modal do pokemon ${numeroDoPokemon}`);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${idDoPokemon}`)
+        .then((response) => response.json())
+        .then(povoarModalDoPokemon)
 }
 
-botaoFecharModal.addEventListener('click', ()=>{
-    modal.close()
+function povoarModalDoPokemon(pokemon) {
+    modal.classList.remove(...modal.classList);
+    modal.classList.add(`modal__container`);
+    modal.classList.add(`${pokemon.types[0].type.name}`)
+
+    nomeDoPokemon.innerHTML = pokemon.name;
+    numeroDoPokemon.innerHTML = `# ${pokemon.id}`;
+
+    tiposDoPokemon.innerHTML = "";
+    pokemon.types.map( tipo =>{
+        tiposDoPokemon.innerHTML += `<span class="type ${tipo.type.name}">${tipo.type.name}</span>`;
+    });
+
+    imagemDoPokemon.src = pokemon.sprites.other.dream_world.front_default;
+    especieDoPokemon.innerHTML = pokemon.species.name;
+    alturaDoPokemon.innerHTML = `${pokemon.height / 10} m`;
+    pesoDoPokemon.innerHTML = `${pokemon.weight} Kg`;
+
+    habilidadesDoPokemon.innerHTML = "";
+    pokemon.abilities.map( habilidade => {
+        const textoAAdicionar = habilidade.ability.name[0].toUpperCase() + habilidade.ability.name.substring(1);
+        if(pokemon.abilities.indexOf(habilidade) < pokemon.abilities.length - 1) {
+            habilidadesDoPokemon.innerHTML += `${textoAAdicionar}, `;
+        } else {
+            habilidadesDoPokemon.innerHTML += `${textoAAdicionar}.`;
+        }
+    });
+
+    vidaDoPokemon.innerHTML = `<span>${pokemon.stats[0].base_stat}</span><span class="description__range"><div style="width: ${pokemon.stats[0].base_stat}%; background: ${pokemon.stats[0].base_stat > 49 ? 'lightgreen' : 'red'};"></div></span>`
+    ataqueDoPokemon.innerHTML = `<span>${pokemon.stats[1].base_stat}</span><span class="description__range"><div style="width: ${pokemon.stats[1].base_stat}%; background: ${pokemon.stats[1].base_stat > 49 ? 'lightgreen' : 'red'};"></div></span>`
+    defesaDoPokemon.innerHTML = `<span>${pokemon.stats[2].base_stat}</span><span class="description__range"><div style="width: ${pokemon.stats[2].base_stat}%; background: ${pokemon.stats[2].base_stat > 49 ? 'lightgreen' : 'red'};"></div></span>`
+    spatkDoPokemon.innerHTML = `<span>${pokemon.stats[3].base_stat}</span><span class="description__range"><div style="width: ${pokemon.stats[3].base_stat}%; background: ${pokemon.stats[3].base_stat > 49 ? 'lightgreen' : 'red'};"></div></span>`
+    spdefDoPokemon.innerHTML = `<span>${pokemon.stats[4].base_stat}</span><span class="description__range"><div style="width: ${pokemon.stats[4].base_stat}%; background: ${pokemon.stats[4].base_stat > 49 ? 'lightgreen' : 'red'};"></div></span>`
+    velDoPokemon.innerHTML = `<span>${pokemon.stats[5].base_stat}</span><span class="description__range"><div style="width: ${pokemon.stats[5].base_stat}%; background: ${pokemon.stats[5].base_stat > 49 ? 'lightgreen' : 'red'};"></div></span>`
+
+    imagemPokemonAntesEvo.src = pokemon.sprites.other.dream_world.front_default;
+    imagemPokemonDepoisEvo.src = pokemon.sprites.other.dream_world.front_default;
+    nomeDaEvoDoPokemon.innerHTML = pokemon
+}
+
+botaoFecharModal.addEventListener('click', ()=>{modal.close()});
+
+arrayStatsTitulo.map( elementoHTML => {
+    elementoHTML.addEventListener('click', (evento)=>{
+        arrayStatsTitulo.map(elemento=> {
+            elemento.classList.remove("item__ativo");
+        })
+        arrayAbasDesc.map( elemento => {
+            elemento.classList.remove("item__ativo");
+        })
+
+        evento.target.classList.add("item__ativo");
+        arrayAbasDesc[arrayStatsTitulo.indexOf(evento.target)].classList.add("item__ativo");
+    })
 })
